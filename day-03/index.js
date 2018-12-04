011fde2ca08ed5813bc21c9466a5ab9716232e14
zehfernandes <ozehfernandes@gmail.com>
2018-12-03 23:51:35 -0200

    adding grid count

--- day-03/index.js
+++ day-03/index.js
@@ -1,5 +1,5 @@
 const firstAnswer = input => {
-	return 1
+  return 1;
 };
 
 const secondAnswer = input => {
@@ -7,17 +7,16 @@ const secondAnswer = input => {
 };
 
 const parser = input => {
-  return input.map(o => {
-      if(o === "") return //FIX THIS FUCK YOU P5 CONVERT STRING
-      const match = /(#\d*)\s@\s(\d*),(\d*):\s(\d*)x(\d*)/g.exec(o);
-      return {
-        id: match[1],
-        x: Number(match[2]),
-        y: Number(match[3]),
-        width: Number(match[4]),
-        height: Number(match[5])
-      };
-    });
+  return input.slice(0, input.length - 1).map(o => {
+    const match = /(#\d*)\s@\s(\d*),(\d*):\s(\d*)x(\d*)/g.exec(o);
+    return {
+      id: match[1],
+      x: Number(match[2]),
+      y: Number(match[3]),
+      width: Number(match[4]),
+      height: Number(match[5])
+    };
+  });
 };
 
 module.exports = {
--- day-03/sketch.js
+++ day-03/sketch.js
@@ -2,78 +2,68 @@ const canvasSketch = require("canvas-sketch");
 const p5 = require("p5");
 const { parser } = require("./index.js");
 
-let data;
-
-const preload = p5 => {
-  data = p5.loadStrings("./day-03/input.txt");
-};
-
-class Point{
-    constructor(x, y){
-      this.x = x;
-      this.y = y;
-    }
-
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
-class Rect{
-  constructor(id, x, y, w, h){
+class Rect {
+  constructor(id, x, y, w, h) {
     this.x = x;
-    this.y =  y;
+    this.y = y;
     this.w = w;
     this.h = h;
     this.id = id;
   }
 }
 
-
 //   p2min        p2max
 //     .------------.
-//     |       | 
+//     |       |
 // .-----------.
 // p1min    p1max
-function overlap1d(p1min, p1max, p2min, p2max){
-
+function overlap1d(p1min, p1max, p2min, p2max) {
   var intersects = p1max >= p2min && p2max >= p1min;
 
-  let a = ( p1min < p2min ) ? p2min : p1min;
-  let b = ( p1max > p2max ) ? p2max : p1max;
+  let a = p1min < p2min ? p2min : p1min;
+  let b = p1max > p2max ? p2max : p1max;
 
   return {
-      intersects : intersects,
-      range : {a: a, b: b}
-    }
+    intersects: intersects,
+    range: { a: a, b: b }
+  };
 }
 
+function overlap2d(rectA, rectB) {
+  pointA = new Point(rectA.x, rectA.y);
+  pointAA = new Point(rectA.x + rectA.w, rectA.y + rectA.h);
 
-function overlap2d( rectA, rectB )  { 
+  pointB = new Point(rectB.x, rectB.y);
+  pointBB = new Point(rectB.x + rectB.w, rectB.y + rectB.h);
 
-    pointA = new Point(rectA.x, rectA.y);
-    pointAA = new Point(rectA.x + rectA.w, rectA.y + rectA.h)
+  A = overlap1d(pointA.x, pointAA.x, pointB.x, pointBB.x);
+  B = overlap1d(pointA.y, pointAA.y, pointB.y, pointBB.y);
 
-    pointB =  new Point(rectB.x, rectB.y);
-    pointBB =  new Point(rectB.x + rectB.w, rectB.y + rectB.h)
-
-    A = overlap1d( pointA.x, pointAA.x, pointB.x, pointBB.x );
-    B = overlap1d( pointA.y, pointAA.y, pointB.y, pointBB.y );
-
-    // console.log(rectA);
-    // console.log(rectB);
-    // console.log(A);
-    // console.log(B);
-
-  if( A.intersects == false || B.intersects == false)  {
+  if (A.intersects == false || B.intersects == false) {
     return null;
   }
-  return new Rect(-1, A.range.a, B.range.a, A.range.b - A.range.a, B.range.b - B.range.a );
 
+  return new Rect(
+    -1,
+    A.range.a,
+    B.range.a,
+    A.range.b - A.range.a,
+    B.range.b - B.range.a
+  );
 }
 
-function drawRect(p5, rect ){
-  p5.rect(rect.x, rect.y, rect.w, rect.h);
-}
+let data;
 
-
+const preload = p5 => {
+  data = p5.loadStrings("./day-03/inputs/input-1.txt");
+};
 
 const settings = {
   p5: { p5, preload },
@@ -83,43 +73,51 @@ const settings = {
 const sketch = async () => {
   const input = parser(data);
   // const input = [
-  //   {id: "#1", x: 1, y: 3, width: 4, height: 4},
-  //   {id: "#2", x: 3, y: 1, width: 4, height: 4},
-  //   {id: "#3", x: 5, y: 5, width: 2, height: 2} 
-  // ]
+  //   { id: "#1", x: 1, y: 3, width: 4, height: 4 },
+  //   { id: "#2", x: 3, y: 1, width: 4, height: 4 },
+  //   { id: "#3", x: 4, y: 4, width: 2, height: 2 },
+  //   { id: "#9", x: 10, y: 10, width: 2, height: 2 }
+  // ];
+
+  let grid = Object.create(null);
 
   return ({ p5, time, width, height }) => {
-
     p5.background(0);
     p5.noStroke();
 
-
-    let overlaping_inches = 0;
-
-    for(var i=0; i<input.length; i++){
+    for (let i = 0; i < input.length; i++) {
       let shape = input[i];
-      let rectA = new Rect(shape.id, shape.x, shape.y, shape.width, shape.height);
-      console.log( "area: ", rectA );
-      // p5.fill(255, 0, 0, 127);
-      // drawRect(p5, rectA);
+      let rectA = new Rect(
+        shape.id,
+        shape.x,
+        shape.y,
+        shape.width,
+        shape.height
+      );
+      p5.fill(255, 0, 0, 127);
+      p5.rect(rectA.x, rectA.y, rectA.w, rectA.h);
 
-      for(var j= i + 1; j<input.length; j++){
+      for (let j = i + 1; j < input.length; j++) {
         let shapeB = input[j];
-        rectB = overlap2d(  rectA, new Rect(shapeB.id, shapeB.x, shapeB.y, shapeB.width, shapeB.height) );
+        rectB = overlap2d(
+          rectA,
+          new Rect(shape.id, shapeB.x, shapeB.y, shapeB.width, shapeB.height)
+        );
 
-        if( rectB !=  null ){
+        if (rectB != null) {
           p5.fill(255, 255, 0);
-          drawRect(p5, rectB );
+          p5.rect(rectB.x, rectB.y, rectB.w, rectB.h);
 
-          let area = rectB.w * rectB.h;
-          overlaping_inches += area;
+          for (let x = rectB.x; x < rectB.x + rectB.w; x++) {
+            for (let y = rectB.y; y < rectB.y + rectB.h; y++) {
+              grid[`${x},${y}`] = 1;
+            }
+          }
         }
       }
-
     }
 
-    console.log( overlaping_inches );
-  
+    console.log(Object.values(grid).filter(v => v == 1).length);
   };
 };
 
